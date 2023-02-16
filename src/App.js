431ca06d7b1caa3ed6e74b8d97718f06dbcbd323
neerajk555 <neeraj.kashikar@live.com>
2023-02-16 07:39:23 -0800

    third

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from './components/PersonList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonList/>
+    </div>
+  )
+}
+
+export default App;
