75936fd9728428d881d3de3504e3b823d7c20950
neerajk555 <neeraj.kashikar@live.com>
2023-02-16 05:12:39 -0800

    initial

---
+++ react-axios-example/src/components/PersonList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+      .then(res => {
+        const persons = res.data.body;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.persons
+            .map(person =>
+              <li key={person.id}>{person.id}{person.name}</li>
+            )
+        }
+      </ul>
+    )
+  }
+}
