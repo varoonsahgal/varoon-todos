988d592f62ccbaf68d280c79376d2c7c7a6042d7
varoonsahgal <varoon.sahgal@gmail.com>
2023-03-13 17:09:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,21 @@ terraform {
 
 data "aws_caller_identity" "current" {}
 
+variable "username" {
+  description = "The username for the DB master user"
+  type        = string
+}
+variable "password" {
+  description = "The password for the DB master user"
+  type        = string
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.username
+  secret_key = var.password
+}
+
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
