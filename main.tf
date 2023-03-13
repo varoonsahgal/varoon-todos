e5b8baa589c5bb665a33c96bb5350bb6612ebb63
varoonsahgal <varoon.sahgal@gmail.com>
2023-03-13 08:57:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,20 @@ terraform {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
 provider "aws" {
   region  = "us-west-2"
 }
